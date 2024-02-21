b4e94ef3f828bb679b7ee5ae31a3d6daa58e1e27
Tristan Döhl <tristan.doehl@holy-mail.de>
2024-02-21 16:16:25 +0100

    Fix: Types

--- src/app/components/Toast/notify.tsx
+++ src/app/components/Toast/notify.tsx
@@ -1,13 +1,10 @@
 import { FC } from 'react';
 import { toast } from 'react-toastify';
-import { ToastProps } from 'react-toastify/dist/types';
 
 
 interface MessageProps {
   title: string;
   description?: string;
-  closeToast?: () => void;
-  toastProps: ToastProps;
 }
 
 const Message: FC<MessageProps> = ({title, description}) => {
@@ -21,15 +18,15 @@ const Message: FC<MessageProps> = ({title, description}) => {
 
 
 export interface Notification {
-  title: string;
+  title: string; 
   description?: string;
 
 }
 
 export const notifyError = ({title, description}: Notification) => {
   toast.error(
-    ({closeToast, toastProps}) =>
-      <Message closeToast={closeToast} toastProps={toastProps} title={title} description={description}/>,
+    () =>
+      <Message  title={title} description={description}/>,
     {
       progress: undefined,
       hideProgressBar: true
@@ -39,8 +36,8 @@ export const notifyError = ({title, description}: Notification) => {
 export const notifySuccess = ({title, description}: Notification) => {
   console.log('toast success')
   toast.success(
-    ({closeToast, toastProps}) =>
-      <Message closeToast={closeToast} toastProps={toastProps} title={title} description={description}/>,
+    () =>
+      <Message title={title} description={description}/>,
     {
       progress: undefined,
       hideProgressBar: true
@@ -49,8 +46,8 @@ export const notifySuccess = ({title, description}: Notification) => {
 
 export const notifyInfo = ({title, description}: Notification) => {
   toast.info(
-    ({closeToast, toastProps}) =>
-      <Message closeToast={closeToast} toastProps={toastProps} title={title} description={description}/>,
+    () =>
+      <Message title={title} description={description}/>,
     {
       progress: undefined,
       hideProgressBar: true
