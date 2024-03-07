d6f8e25f0e0339eecf11fdce18de5c600924e497
Tristan Döhl <tristan.doehl@holy-mail.de>
2024-03-07 13:45:23 +0100

    Fix: Run wepback on 1 core

--- next.config.js
+++ next.config.js
@@ -7,6 +7,9 @@ await import("./src/env.js");
 /** @type {import("next").NextConfig} */
 const config = {
   experimental: {
+  },
+  webpack: (config) => {
+    return {...config, parallelism: 1};
   }
 };
 
