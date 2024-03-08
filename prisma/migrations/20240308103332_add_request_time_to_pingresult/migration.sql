d76cb13bfee63f506b48ae71fed53c7fe5c785f9
Tristan Döhl <tristan.doehl@holy-mail.de>
2024-03-08 13:42:05 +0100

    Feat: Added request duration and display it as chart

---
+++ prisma/migrations/20240308103332_add_request_time_to_pingresult/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "PingResult" ADD COLUMN     "requestTime" INTEGER NOT NULL DEFAULT 0;
---
+++ prisma/migrations/20240308103347_add_request_time_to_pingresult_nodefault/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "PingResult" ALTER COLUMN "requestTime" DROP DEFAULT;
